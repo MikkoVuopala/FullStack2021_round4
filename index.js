31d76ee53ab502901f1b2cf6e80e737f2a9a2859
Mikko Vuopala <mikko.vuopala@aalto.fi>
2021-06-01 11:45:52 +0300

    4.2 tehtynä (sovellus jaettu moduuleihin)

--- index.js
+++ index.js
@@ -1,25 +1,29 @@
 const http = require('http')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const app = require('./app')
 const express = require('express')
-const app = express()
+/*const app = express()
 const cors = require('cors')
-const mongoose = require('mongoose')
+const mongoose = require('mongoose')*/
+const server = http.createServer(app)
 
-const blogSchema = mongoose.Schema({
+/*const blogSchema = mongoose.Schema({
   title: String,
   author: String,
   url: String,
   likes: Number
 })
 
-const Blog = mongoose.model('Blog', blogSchema)
+const Blog = mongoose.model('Blog', blogSchema)*/
 
-const mongoUrl = `mongodb+srv://fsround4:<password>@fsblog.koloc.mongodb.net/blog?retryWrites=true&w=majority`
+/*const mongoUrl = config.MONGODB_URI
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
 
 app.use(cors())
-app.use(express.json())
+app.use(express.json())*/
 
-app.get('/api/blogs', (request, response) => {
+/*app.get('/api/blogs', (request, response) => {
   Blog
     .find({})
     .then(blogs => {
@@ -35,9 +39,8 @@ app.post('/api/blogs', (request, response) => {
     .then(result => {
       response.status(201).json(result)
     })
-})
+})*/
 
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+server.listen(config.PORT, () => {
+  console.log(`Server running on port ${config.PORT}`)
 })
