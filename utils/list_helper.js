f17aa209922c21edb9b8cdc02ff5de742fbd6b3b
Mikko Vuopala <mikko.vuopala@aalto.fi>
2021-06-05 16:09:32 +0300

    4.5 tehty

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -11,7 +11,21 @@ const totalLikes = (blogs) => {
         : blogs.reduce(reducer, 0)
 }
 
+const favoriteBlog = (blogs) => {
+    const arr = []
+    for (i = 0; i < blogs.length; i++) {
+        arr.push(blogs[i].likes)
+    }
+    const maxLikes = Math.max(...arr)
+    const fave = blogs.find(b => b.likes === maxLikes)
+    
+    return blogs.length === 0
+        ? null
+        : fave
+}
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
